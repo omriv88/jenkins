bfef309bce7fa387f1aed20f1d4ecbe14a90b17a
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-01-31 23:00:57 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl --insecure http://localhost/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar
 chmod +x jenkins-cli.jar
-java -jar jenkins-cli.jar -s http://localhost:8080 -auth admin:password -webSocket create-job "Run Python Script when any pull requests in github" < pipeline.xml
-java -jar jenkins-cli.jar -s http://localhost:8080 -auth admin:password -webSocket create-job "test2" < test2.xml
+java -jar jenkins-cli.jar -s http://localhost:8080 -auth admin:admin -webSocket create-job "Run Python Script when any pull requests in github" < pipeline.xml
+java -jar jenkins-cli.jar -s http://localhost:8080 -auth admin:admin -webSocket create-job "test2" < test2.xml
