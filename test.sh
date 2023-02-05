a597341a6fbad57719b2d7f7d6db8015f4379abe
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-02-05 04:47:14 +0200

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl --insecure http://54.160.65.219:8080/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar
-chmod +x jenkins-cli.jar
-java -jar jenkins-cli.jar -s http://54.160.65.219:8080 -webSocket create-job "pull request demo" < freestyle-job.xml
-java -jar jenkins-cli.jar -s http://54.160.65.219:8080 -webSocket create-job "pipeline" < pipeline-job.xml
