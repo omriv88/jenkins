426d12bc15d76fd5788b241ba10378c2369249d2
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-02-05 05:12:36 +0200

    Update play.sh

--- play.sh
+++ play.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 git clone "https://github.com/omriv88/deploy-jenkins-jobs.git"
-pws
+cp /root/test/jenkins/deploy-jenkins-jobs/* /root/test/jenkins/
+
 docker build -t myjenkins .
 
 sudo /usr/local/bin/docker-compose -f "/root/project/docker-compose.yaml" up -d
 
 sleep 30s
 
-bash test.sh
+bash  test.sh
