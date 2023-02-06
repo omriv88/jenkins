236a48225228e3884d8a6411777ea2fc5560ab9f
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-02-06 11:58:55 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ git clone "https://github.com/omriv88/deploy-jenkins-jobs.git"
 cp ./deploy-jenkins-jobs/* ./
 docker build -t myjenkins .
 sudo /usr/local/bin/docker-compose -f "./docker-compose.yaml" up -d
-sleep 30s
+sleep 40s
 bash  test.sh
